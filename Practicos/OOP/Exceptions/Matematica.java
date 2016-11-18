5956e358238f654b99cd354e9d6c771641a6117a
Nicolas <nerynicolas@gmail.com>
2016-11-18 12:06:03 -0300

    Some notes
    About what's happening in this code

--- Practicos/OOP/Exceptions/Matematica.java
+++ Practicos/OOP/Exceptions/Matematica.java
@@ -4,7 +4,9 @@ package main.java.com.frre.tsp.exceptions;
  * Expone metodos matematicos
  */
 public class Matematica {
-
+    
+    //it cactches the exception
+    //and them return and integer, like a code style
     public int dividir(int dividendo, int divisor) {
 
         try {
@@ -14,11 +16,12 @@ public class Matematica {
             return -1;
         }
     }
-
+    //no se trata la excepción de ninguna forma
     public double dividir(double dividendo, double divisor) {
         return dividendo / divisor;
     }
-
+    
+    //simply catching
     public int dividir2(int dividendo, int divisor) {
 
         try {
@@ -28,7 +31,8 @@ public class Matematica {
             throw e;
         }
     }
-
+    //catching and
+    //rethrowing the exception to other kind
     public int dividir3(int dividendo, int divisor) throws DivisionByZeroException {
 
         try {
